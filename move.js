dd8dcffcb775be761a624db3827770909b68a581
ItsJonQ <itsjonq@gmail.com>
2018-02-18 18:27:13 -0500

    Refactored with classes

--- move.js
+++ move.js
@@ -1,17 +1,17 @@
-const meMoveEvent = new Event('me:move')
-const MAX_THEM = 100
-let SCORE = 0
-let FASTEST_THEM = 400
+!function(n,e){"object"==typeof exports&&"undefined"!=typeof module?module.exports=e():"function"==typeof define&&define.amd?define(e):n.mitt=e()}(this,function(){function n(n){return n=n||Object.create(null),{on:function(e,t){(n[e]||(n[e]=[])).push(t)},off:function(e,t){n[e]&&n[e].splice(n[e].indexOf(t)>>>0,1)},emit:function(e,t){(n[e]||[]).slice().map(function(n){n(t)}),(n["*"]||[]).slice().map(function(n){n(e,t)})}}}return n});
+//# sourceMappingURL=mitt.umd.js.map
+
+const GEM = mitt()
+
+let SCORE = null
+let ME = null
+
+let THEM_COUNT = 0
+let THEM_MAX = 100
+let THEM_LIST = []
+
+let FASTEST_THEM = 500
 let SLOWEST_THEM = 5000
-
-const Score = document.createElement('div')
-Score.classList.add('Score')
-document.body.append(Score)
-
-const Me = document.createElement('div')
-Me.classList.add('Entity', 'Me')
-document.body.append(Me)
-const MeBCR = Me.getBoundingClientRect()
 
 const getRandom = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min
@@ -25,143 +25,218 @@ const getRandomScreenY = () => {
   return getRandom(0, window.innerHeight)
 }
 
-let themCount = 0
+class Score {
+  constructor () {
+    this.countNode = null
+    this.count = 0
+    this.step = 0
+    this.mount()
+  }
 
-const addThem = () => {
-  themCount = themCount + 1
-  if (themCount > MAX_THEM) return
-  const Them = document.createElement('div')
-  Them.classList.add('Entity', 'Them')
-  Them.style.transitionDuration = `${getRandom(FASTEST_THEM, SLOWEST_THEM)}ms`
-  Them.style.top = `${getRandomScreenX()}px`
-  Them.style.right = `${getRandomScreenY()}px`
-  Them.classList.add('them')
-  document.body.append(Them)
-  const BCR = Them.getBoundingClientRect()
-  const { top, left } = BCR
-  Them.setAttribute('data-top', top)
-  Them.setAttribute('data-left', left)
+  increment () {
+    this.count += 1
+    this.adjustDifficulty()
+    this.render()
+  }
 
-  Them.addEventListener('transitionend', maybeCatchMe)
+  adjustDifficulty () {
+    const c = this.count
+    if (c > 20 && this.step === 0) {
+      SLOWEST_THEM -= 1000
+      this.step = 1
+    }
+    if (c > 50 && this.step === 1) {
+      SLOWEST_THEM -= 1000
+      this.step = 2
+    }
+    if (c > 70 && this.step === 2) {
+      SLOWEST_THEM -= 1000
+      this.step = 3
+    }
+  }
+
+  mount () {
+    this.node = document.createElement('div')
+    this.node.classList.add('Score')
+    document.body.append(this.node)
+
+    this.countNode = document.createElement('div')
+    this.countNode.classList.add('ScoreCount')
+    this.node.append(this.countNode)
+    this.render()
+  }
+
+  render () {
+    this.countNode.innerHTML = this.count
+  }
 }
 
-const repositionMe = (event) => {
-  const { pageX, pageY } = event
-  const x = pageX - (MeBCR.width / 2) - MeBCR.left
-  const y = pageY - (MeBCR.height / 2) - MeBCR.top
-  requestAnimationFrame(() => {
-    Me.style.opacity = 1
-    Me.style.transform = `translate(${x}px, ${y}px)`
-    const BCR = Me.getBoundingClientRect()
-    Me.setAttribute('data-current-x', BCR.left)
-    Me.setAttribute('data-current-y', BCR.top)
+class Entity {
+  constructor () {
+    this.node = null
+    this.BCR = {}
+
     requestAnimationFrame(() => {
-      Me.dispatchEvent(meMoveEvent)
+      this.willMount()
+      this.mount()
+      this.didMount()
     })
-  })
+  }
+
+  mount () {
+    this.node = document.createElement('div')
+    document.body.append(this.node)
+
+    this.node.classList.add('Entity')
+    this.BCR = this.node.getBoundingClientRect()
+  }
+
+  didMount () {}
+  willMount () {}
 }
 
-const repositionThem1 = (event) => {
-  const me = event.target
-  const currentBCR = me.getBoundingClientRect()
-  const { x, y } = currentBCR
-  const ThemNodes = document.querySelectorAll('.them')
-  ThemNodes.forEach(n => {
-    n.style.opacity = 1
-    const left = n.getAttribute('data-left')
-    const top = n.getAttribute('data-top')
+class Me extends Entity {
+  constructor () {
+    super ()
+    this.reposition = this.reposition.bind(this)
+    this.fire = this.fire.bind(this)
+    this.x = null
+    this.y = null
+    window.addEventListener('mousemove', this.reposition)
+    window.addEventListener('click', this.fire)
+  }
+
+  didMount () {
+    this.node.classList.add('Me')
+  }
+
+  reposition (event) {
+    const { pageX, pageY } = event
+    const x = pageX - (this.BCR.width / 2)
+    const y = pageY - (this.BCR.height / 2)
+    this.node.style.opacity = 1
+
+    requestAnimationFrame(() => {
+      this.x = x
+      this.y = y
+      this.node.style.transform = `translate(${x}px, ${y}px)`
+      GEM.emit('meMove', this)
+    })
+  }
+
+  fire (event) {
+    const { pageX, pageY } = event
+    let closestThem = THEM_LIST.filter(t => !t.isDestroyed)
+    if (!closestThem.length) return
+
+    closestThem = closestThem
+      .reduce((prev, curr) => {
+        const pBCR = prev.node.getBoundingClientRect()
+        const cBCR = curr.node.getBoundingClientRect()
+        const pX = pBCR.left
+        const pY = pBCR.top
+        const cX = cBCR.left
+        const cY = cBCR.top
+        return (
+          (
+            Math.abs(cX - pageX) < Math.abs(pX - pageX) &&
+            Math.abs(cY - pageY) < Math.abs(pY - pageY)
+          )
+          ? curr : prev
+        )
+      })
+
+    if (closestThem) {
+      closestThem.destroy()
+    }
+  }
+}
+
+class Them extends Entity {
+  constructor (props) {
+    super()
+    this.top = null
+    this.left = null
+    this.target = props.target
+    this.followMe = this.followMe.bind(this)
+    this.handleMeMove = this.handleMeMove.bind(this)
+    this.maybeCatchMe = this.maybeCatchMe.bind(this)
+    this.destroy = this.destroy.bind(this)
+    this.isDestroyed = false
+
+    GEM.on('meMove', this.handleMeMove)
+  }
+
+  didMount () {
+    this.node.classList.add('Them')
+    this.node.style.transitionDuration = `${getRandom(FASTEST_THEM, SLOWEST_THEM)}ms`
+    this.node.style.top = `${getRandomScreenX()}px`
+    this.node.style.right = `${getRandomScreenY()}px`
+    this.node.addEventListener('transitionend', this.maybeCatchMe)
+    this.offsetTop = this.node.offsetTop
+    this.offsetLeft = this.node.offsetLeft
+
+    this.followMe()
+  }
+
+  followMe () {
+    const currentBCR = this.target.node.getBoundingClientRect()
+    const { x, y } = currentBCR
+
+    const left = this.node.offsetLeft
+    const top = this.node.offsetTop
     const tX = x - left
     const tY = y - top
     requestAnimationFrame(() => {
-      n.style.transform = `translate(${tX}px, ${tY}px)`
+      this.node.style.transform = `translate(${tX}px, ${tY}px)`
     })
-  })
-}
-
-const maybeCatchMe = (event) => {
-  const node = event.target
-  const BCR = node.getBoundingClientRect()
-  const t = 25
-  const x = Math.round(BCR.left) - t
-  const y = Math.round(BCR.top) - t
-  const mX = parseInt(Me.getAttribute('data-current-x'), 10)
-  const mY = parseInt(Me.getAttribute('data-current-y'), 10)
-
-  if (
-    mX >= (x - t) && mX <= (x + t + t) &&
-    mY >= (y - t) && mY <= (y + t + t)
-  ) {
-    window.location.reload()
   }
-}
 
-const destroyThem = (event) => {
-  const { pageX, pageY } = event
-  let closestThem = Array.from(document.querySelectorAll('.them'))
-  if (!closestThem.length) return
+  handleMeMove () {
+    this.node.style.opacity = 1
+    this.followMe()
+  }
 
-  closestThem = closestThem
-    .reduce((prev, curr) => {
-      const pBCR = prev.getBoundingClientRect()
-      const cBCR = curr.getBoundingClientRect()
-      const pX = pBCR.left
-      const pY = pBCR.top
-      const cX = cBCR.left
-      const cY = cBCR.top
-      return (
-        (
-          Math.abs(cX - pageX) < Math.abs(pX - pageX) &&
-          Math.abs(cY - pageY) < Math.abs(pY - pageY)
-        )
-        ? curr : prev
-      )
-    })
+  maybeCatchMe (event) {
+    const BCR = this.node.getBoundingClientRect()
+    const t = 25
+    const x = Math.round(BCR.left) - t
+    const y = Math.round(BCR.top) - t
+    const mX = parseInt(this.target.x, 10)
+    const mY = parseInt(this.target.y, 10)
 
-  if (closestThem) {
+    if (
+      mX >= (x - t) && mX <= (x + t + t) &&
+      mY >= (y - t) && mY <= (y + t + t)
+    ) {
+      this.node.style.opacity = 1
+      requestAnimationFrame(() => {
+        window.location.reload()
+      })
+    }
+  }
+
+  destroy () {
+    if (this.isDestroyed) return
+    SCORE.increment()
+    THEM_COUNT -= 1
+    this.isDestroyed = true
     requestAnimationFrame(() => {
-      document.body.removeChild(closestThem)
-      themCount = themCount - 1
-      SCORE = SCORE + 1
-      renderScore()
+      document.body.removeChild(this.node)
     })
   }
 }
 
-const resetThemCoords = () => {
-  let themNodes = Array.from(document.querySelectorAll('.them'))
-  themNodes.map(n => {
-    requestAnimationFrame(() => {
-      const t = n.getAttribute('data-top')
-      const l = n.getAttribute('data-left')
-      const ot = n.offsetTop
-      const ol = n.offsetLeft
-      if (t === ot || l === ol) return
-      n.setAttribute('data-top', ot)
-      n.setAttribute('data-left', ol)
-    })
-  })
+const addThem = () => {
+  THEM_COUNT = THEM_COUNT + 1
+  if (THEM_COUNT > THEM_MAX) return
+  THEM_LIST.push(new Them({ target: ME }))
 }
 
-const renderScore = () => {
-  Score.innerHTML = SCORE
+const start = () => {
+  ME = new Me()
+  SCORE = new Score()
+  setInterval(addThem, 500)
 }
 
-const adjustDifficulty = () => {
-  if (SCORE > 20) {
-    SLOWEST_THEM =- 1000
-  }
-  if (SCORE > 50) {
-    SLOWEST_THEM =- 1000
-  }
-  if (SCORE > 70) {
-    SLOWEST_THEM =- 1000
-  }
-}
-
-window.addEventListener('mousemove', repositionMe)
-window.addEventListener('click', destroyThem)
-window.addEventListener('resize', resetThemCoords)
-Me.addEventListener('me:move', repositionThem1)
-
-renderScore()
-setInterval(addThem, 500)
+start()
